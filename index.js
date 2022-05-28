e3b5a6ec43c066e5cd32f270ba30a360a3aea030
Abu Naem <88497497+abunaem123@users.noreply.github.com>
2022-05-28 21:15:23 +0600

    jwt connected

--- index.js
+++ index.js
@@ -97,6 +97,9 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('running Car Manufacturer server');
 });
+app.get('/', (req, res) => {
+    res.send('running Car Manufacturer server');
+});
 
 app.listen(port, ()=> {
     console.log('Car Manufacturer listening on port ${port}');
